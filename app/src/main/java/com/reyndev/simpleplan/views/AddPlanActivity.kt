3339cb74e6c919f50c85bfb451bb42f6f8d78d19
ReynaldyNC <reynaldy.nur.cahyo.003@gmail.com>
2023-08-05 12:57:59 +0700

    Update
    New
    - Added setOnClickListener for "btnBack" and "btnAddPlan"

--- app/src/main/java/com/reyndev/simpleplan/views/AddPlanActivity.kt
+++ app/src/main/java/com/reyndev/simpleplan/views/AddPlanActivity.kt
@@ -2,11 +2,40 @@ package com.reyndev.simpleplan.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.Toast
 import com.reyndev.simpleplan.R
 
 class AddPlanActivity : AppCompatActivity() {
+    private lateinit var btnBack: ImageButton
+    private lateinit var btnAddPlan: Button
+    private lateinit var edtTitle: EditText
+    private lateinit var edtDesc: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_plan)
+
+        edtTitle = findViewById(R.id.edtAddTitle)
+        edtDesc = findViewById(R.id.edtAddDesc)
+
+        btnBack = findViewById(R.id.btnAddBack)
+        btnBack.setOnClickListener {
+            finish()
+        }
+
+        btnAddPlan = findViewById(R.id.btnAddSubmit)
+        btnAddPlan.setOnClickListener {
+            Toast.makeText(
+                this,
+                "Name: ${edtTitle.text} | Desc: ${edtDesc.text}",
+                Toast.LENGTH_SHORT
+            ).show()
+
+            finish()
+        }
+
     }
 }
